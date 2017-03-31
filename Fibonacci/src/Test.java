9e7df932f3453f9a9fc078df2cfb5db5c59d9d4a
Francesco Vicidomini <fr.vicidomini94@gmail.com>
2017-03-31 22:33:59 +0200

    Update Test.java

--- Fibonacci/src/Test.java
+++ Fibonacci/src/Test.java
@@ -8,14 +8,10 @@ public class Test{
 		System.out.println("Witch Fibonacci's number do you want calcolate: ");
 		int n;
 		n=in.nextInt();
-		try{
+		
 		Fibonacci fib=new Fibonacci(n);
 		System.out.println("Fibonacci's("+n+")= "+fib.calcolate(n));
-		}catch(NegativeArraySizeException e){
-			System.err.println("Length of Array < 0");
-			e.printStackTrace();
-			System.exit(0);
-		}
+		
 
 	}
 }
